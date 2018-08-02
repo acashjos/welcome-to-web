6300201a0c29bdded275adfe370b19f6c3049fb9
acashjos <acash_92@live.com>
2018-08-02 02:10:09 -0400

    koa app init

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-var http = require('http');
+const Koa = require('koa');
+const app = new Koa();
 
-var reqHandler = function (req, res) {
-    res.write('Hello World! you are on page'+req.url); //write a response to the client
-    res.end(); //end the response
-  };
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
 
-//create a server object:
-var server = http.createServer(reqHandler);
-server.listen(8080); //the server object listens on port 8080 
-
-
-/*
-
-// Or you can run everything in a single go like this:
-
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080 
-*/
+app.listen(8080);
